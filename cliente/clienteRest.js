b62fa51b3d25408aa94aea2c48d9850d0e35c23e
Yasin Munoz <yasinmunoz@hotmail.com>
2022-10-22 12:07:02 +0200

    cookies

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -6,52 +6,51 @@ function ClienteRest() {
 
         let cli = this;
         $.getJSON("/agregarUsuario/" + nick, function (data) {
-            
+
             //se ejecuta cuando conteste el servidor
             console.log(data);
-            
+
             if (data.nick != -1) {
-                console.log("Usuario " + data.nick + " registrado")
+                console.log("Usuario " + data.nick + " registrado");
                 cli.nick = data.nick;
                 //ws.nick=data.nick;
-                //$.cookie("nick",ws.nick);
-                cli.obtenerListaPartidasDisponibles();
+                $.cookie("nick", data.nick);
                 iu.mostrarHome();//iu.mostrarHome(data.nick)
             }
             else {
-                console.log("No se ha podido registrar el usuario")
-                //iu.mostrarModal("El nick ya está en uso");
+                console.log("No se ha podido registrar el usuario");                
+                iu.mostrarModal("El nick ya está en uso");
                 iu.mostrarAgregarUsuario();
             }
         });
     };
 
     this.crearPartida = function () {
-        
+
         let cli = this;
         let nick = cli.nick;
-        
+
         $.getJSON("/crearPartida/" + nick, function (data) {
             console.log(data);
             if (data.codigo != -1) {
-                console.log("Usuario " + nick + " crea partida codigo: " + data.codigo)
+                console.log("Usuario " + nick + " crea partida codigo: " + data.codigo);
                 iu.mostrarCodigo(data.codigo);
                 //ws.nick=data.nick;
                 //$.cookie("nick",ws.nick);
                 //iu.mostrarHome(data);
             }
             else {
-                console.log("No se ha podido crear partida")
+                console.log("No se ha podido crear partida");
                 //iu.mostrarModal("El nick ya está en uso");
                 //iu.mostrarAgregarJugador();
             }
         });
     };
-    
+
     this.unirseAPartida = function (codigo) {
-        
+
         let cli = this;
-        
+
         $.getJSON("/unirseAPartida/" + cli.nick + "/" + codigo, function (data) {
             //se ejecuta cuando conteste el servidor
             //console.log(data);
@@ -63,7 +62,7 @@ function ClienteRest() {
                 //iu.mostrarHome(data);
             }
             else {
-                console.log("No se ha podido unir a partida")
+                console.log("No se ha podido unir a partida");
                 //iu.mostrarModal("El nick ya está en uso");
                 //iu.mostrarAgregarJugador();
             }
@@ -71,9 +70,9 @@ function ClienteRest() {
     };
 
     this.obtenerListaPartidas = function () {
-        
+
         let cli = this;
-        
+
         //obtenerPartidasDisponibles
         $.getJSON("/obtenerPartidas", function (lista) {
             console.log(lista);
@@ -82,9 +81,9 @@ function ClienteRest() {
     };
 
     this.obtenerListaPartidasDisponibles = function () {
-        
+
         let cli = this;
-        
+
         $.getJSON("/obtenerPartidasDisponibles", function (lista) {
             console.log(lista);
             iu.mostrarListaDePartidasDisponibles(lista);
